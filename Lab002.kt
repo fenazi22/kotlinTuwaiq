5531caf8aa3dfdc6fcead9be5b13fedcc2f9f971
Faisal Abdullah Alanazi <fenazi22@gmail.com>
2022-10-04 16:54:38 +0300

    Update Lab002.kt

--- Lab002.kt
+++ Lab002.kt
@@ -1,12 +1,14 @@
 package com.example.kotlinbasics.assignments
 
+import android.os.Trace
+
 fun main() {
-//    println("---------- Q1 -------------------")
-//    for (number in 1..100){
-//        // this code if you want continue after print message
-//        println(number)
-//        if (number == 70 )
-//         println("\"IT'S OVER 70!!!\"")
+    println("---------- Q1 -------------------")
+    for (number in 1..100) {
+        // this code if you want continue after print message
+        println(number)
+        if (number == 70)
+            println("\"IT'S OVER 70!!!\"")
 
         //this code for after number 71 it will stop
         //if (number<71)
@@ -14,30 +16,25 @@ fun main() {
         //else{
         // println("\"IT'S OVER 70!!!\"")
         //   break
-        //}
-        println("---------- Q2 -------------------")
-        var i=true
-    var temperature=20
-    while (i){
-    when(temperature){
-        30-> {println("It’s Hot" )
-            i=false
-        }
-        20-> {println("It’s Comfy")
-        i=false
-        }
-        15-> {
-            println("IT'S COLD!!! " )
-            i=false
-        }
-        else -> {println("just the temperature value")
-        i=false
-        }
-
     }
-    }
+    println("---------- Q2 -------------------")
+//    var temperature = 35
+//    while (temperature>=10) {
+//        when (temperature) {
+//            30 -> println("It’s Hot")
+//
+//            20 -> println("It’s Comfy")
+//
+//            15 -> println("IT'S COLD!!! ")
+//
+//            else -> println("just the temperature $temperature")
+
+
+//        }
+//        temperature--
+//    }
+
+}
 
 
 
-
-    }
