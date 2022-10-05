e99f04bfceb06ddb9a7e6bdfc941026b60103091
Faisal Abdullah Alanazi <fenazi22@gmail.com>
2022-10-05 10:46:04 +0300

    Update Lab003.kt

--- Lab003.kt
+++ Lab003.kt
@@ -1,14 +1,14 @@
 package com.example.kotlinbasics.assignments
 
 fun main() {
-    var sum=0.0
+       var sum=0.0
 
     var num= arrayListOf<Double>(1.0 , 2.0, 3.0, 4.0, 5.0)
-    var avg=num.size
+    var arraySize=num.size
     for (i in 0 until  num.size){
         sum+=num[i]
     }
 
-    println("average is :"+(sum/avg))
+    println("average is :"+(sum/arraySize))
 
 }
